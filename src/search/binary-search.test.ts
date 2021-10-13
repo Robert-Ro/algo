83da39662b5fac41df618b4805f9e7004a3e4921
liutsing <liutsingluo@gmail.com>
2021-10-13 22:36:31 +0800

    feat: ✨二分查找

--- src/search/binary-search.test.ts
+++ src/search/binary-search.test.ts
@@ -2,10 +2,9 @@ import { binarySearch } from './binary-search'
 
 describe('binnary search', () => {
   it("9 's index is 4", () => {
-    binarySearch([-1, 0, 3, 5, 9, 12], 9)
-    // expect(binarySearch([-1, 0, 3, 5, 9, 12], 9)).toEqual(4)
+    expect(binarySearch([-1, 0, 3, 5, 9, 12], 9)).toEqual(4)
   })
   it('2 not exist in array', () => {
-    // expect(binarySearch([-1, 0, 3, 5, 9, 12], 2)).toEqual(-1)
+    expect(binarySearch([-1, 0, 3, 5, 9, 12], 2)).toEqual(-1)
   })
 })
--- src/search/binary-search.ts
+++ src/search/binary-search.ts
@@ -1,6 +1,21 @@
+/**
+ * 有序数组二分查找
+ * https://ethsonliu.com/2018/04/binary-search.html
+ * https://leetcode-cn.com/study-plan/algorithms/?progress=r933vah Q1
+ * @param nums
+ * @param target
+ * @returns
+ */
 export function binarySearch(nums: number[], target: number): number {
-  // const length = nums.length
-  // const leftNums = nums
-  console.log(nums, target)
-  return 0
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const middle = left + ((right - left) >> 1) // 防止溢出，移位也更高效。同时，每次循环都需要更新。
+    if (nums[middle] > target) right = middle - 1
+    else if (nums[middle] < target) left = middle + 1
+    else return middle
+  }
+
+  return -1
 }
