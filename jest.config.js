f2062c26119519c61597a6bcfd131cab3892ecaf
liutsing <liutsingluo@gmail.com>
2021-11-29 18:13:34 +0800

    pref: ⚡️使用@swc-node/jest做测试

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules'],
   roots: ['<rootDir>/src'],
   modulePaths: ['src'],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc-node/jest'],
   },
 }
